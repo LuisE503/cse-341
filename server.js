b1d2298a068a2ef679bd8862fbf45baf269fbe03
LuisE503 <luise503@github.com>
2026-03-13 11:01:04 -0600

    W02: add POST PUT DELETE and Swagger docs

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const mongodb = require('./db/connect');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Contacts API is running. Visit /api-docs for documentation.');
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', require('./routes/index'));
 
 mongodb.initDb((err) => {
